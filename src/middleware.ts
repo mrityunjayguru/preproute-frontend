da5c7478a05db726a71cada17c5ae3ecfe271a50
vikash-creative <code@creativedemonz.com>
2025-12-18 21:34:08 +0530

    comit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,10 @@
 
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/exam/:path*"],
+};
