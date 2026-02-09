a80e404d847847fe76b78564feb21d0b0ec9d287
vikash-creative <code@creativedemonz.com>
2026-02-09 18:00:31 +0530

    commit

--- src/api/Repository.ts
+++ src/api/Repository.ts
@@ -3,11 +3,12 @@ import axios from "axios";
 // const baseUrl="http://147.93.19.155:3200/api/"
 //  const baseUrl="http://localhost:3200/api/"
 //  const baseUrl="https://prodbackend.thepreproute.com/api/"
- const baseUrl = "testbackend.thepreproute.com/api/"
+ const baseUrl = "https://testbackend.thepreproute.com/api/"
 
 const Repository: AxiosInstance = axios.create({
   baseURL: baseUrl,
 });
+
 // Add a request interceptor to set the Authorization header
 Repository.interceptors.request.use(
   (config) => {
