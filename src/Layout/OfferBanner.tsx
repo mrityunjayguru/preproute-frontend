853eb4a55a9da75b4f82c96bb3cc0c8d4ea49e64
vikash-creative <code@creativedemonz.com>
2026-02-13 19:33:37 +0530

    commit

--- src/Layout/OfferBanner.tsx
+++ src/Layout/OfferBanner.tsx
@@ -4,12 +4,12 @@ import React from "react";
 const OfferBanner = () => {
   const router=useRouter()
   return (
-    <div onClick={()=>router.push("/PlanandPricing")} className="w-full cursor-pointer bg-[#EFF5FF] py-1 text-center text-xs font-medium">
+    <div onClick={()=>router.push("/PlanandPricing")} className="w-full cursor-pointer py-3 bg-[#EFF5FF] py-1 text-center text-xs font-medium">
       <span className="text-[#006DFF] font-semibold cursor-pointer hover:underline">
         2 days left! Get 20% OFF
       </span>
       <span className="text-[#000000]">
-        {" "}on the Deluxe plan – Use Coupon code:{" "}
+        {" "}on the PRO plan – Use Coupon code:{" "}
       </span>
       <span className="text-[#006DFF] font-semibold cursor-pointer hover:underline">
         NEW20
