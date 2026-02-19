2b6c7692a553cfa8c2c6a24a464c474409453721
vikash-creative <code@creativedemonz.com>
2026-02-19 11:31:44 +0530

    commit

--- src/Utils/YoutubeSort.ts
+++ src/Utils/YoutubeSort.ts
@@ -1,5 +1,5 @@
-const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
-const CHANNEL_ID = process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID;
+const API_KEY = "AIzaSyCKz-8HDvqIlDlAeN9C2GNMVTfH5_ygFJw";
+const CHANNEL_ID = "UCVggB-6tzozUdHAJqzWsQBQ";
 
 export const fetchYouTubeVideos=async()=> {
   const response = await fetch(
