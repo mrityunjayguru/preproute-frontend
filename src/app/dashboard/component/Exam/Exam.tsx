2df08f4815965a1bfb55ff9e5804cf7d95ade5e1
vikash-creative <code@creativedemonz.com>
2026-03-10 20:14:43 +0530

    commit

--- src/app/dashboard/component/Exam/Exam.tsx
+++ src/app/dashboard/component/Exam/Exam.tsx
@@ -94,7 +94,7 @@ const Exam: React.FC = () => {
   const [isSection, setIsSection] = useState<boolean>(true);
   const [numberOfQuestion, setNumberOfQuestion] = useState<number>(0);
   const [selectedQuestion, setSelectedQuestion] = useState("");
-  const [questionBankId, setQuestionBankId] = useState<any[]>([]);
+  const [questionBankId, setQuestionBankId] = useState<any>(null);
   const [note,setNote]=useState<string>("");
   const [options, setOptions] = useState<Option[]>([
     { id: 1, text: "", isCorrect: true, label: "1st" },
